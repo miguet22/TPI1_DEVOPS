f71a4c1e417f2d2fe99695ffbdeabc81a34990ed
Miguel Tourn <joamigue22@gmail.com>
2026-09-12 14:54:44 -0300

    error sacado para prueba

--- tests/test_ci_demo.py
+++
@@ -1,3 +0,0 @@
-def test_ci_failure_demo():
-    """Fallo temporal para comprobar el CI y su notificación a Discord."""
-    assert False, "Fallo intencional de demostración del CI. Eliminar este archivo después de la prueba."
